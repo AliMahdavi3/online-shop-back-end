9d3c8152f304b332da2f9c1d3e1a7e4bc41e87c2
AliMahdavi3 <alimahdavi30000@gmail.com>
2023-05-12 13:10:40 +0330

    ejs-template

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const path = require("path");
-
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
+app.set("view engine", "ejs");
 app.set("views", "views");
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use("/admin", adminRouter);
+app.use(shopRouter);
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.listen(3000, () => {
-    console.log("Runnig On Port 3000....!");
+mongoose.connect("mongodb://localhost:27017/shop").then((result) => {
+    app.listen(3000, () => {
+        console.log("Listening On Port 3000");
+    })
+}).catch((error) => {
+    console.log(error.message);
 });
