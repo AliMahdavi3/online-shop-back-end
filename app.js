77cdb496ce7aba5cc8d18f4ae82fda2610b0e405
AliMahdavi3 <alimahdavi30000@gmail.com>
2023-05-12 04:07:04 +0330

    create-first-template

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("Runnig On Port 3000....!");
+});
